ee0531b30dbdd1cffd4517ee9152eb464f7f28b1
singhiam15 <singhiam@outlook.com>
2023-05-13 15:30:54 +0530

    updated main file

--- AWS/main.tf
+++ AWS/main.tf
@@ -1,4 +1,11 @@
-resource "aws_instance" "myinstance" {
+resource "aws_instance" "myinstance_east" {
+    provider = selfaws.selfaws_east
     ami = "ami-005b11f8b84489615"
     instance_type = "t2.micro"
+}
+
+resource "aws_instance" "myinstance_west" {
+  provider = selfaws.selfaws_west
+  ami = "ami-051ed863837a0b1b6"
+  instance_type = "t2.micro"
 }

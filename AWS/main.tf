7c8c846cdd5ed3434544142a165e2b8042f96ddb
singhiam15 <singhiam@outlook.com>
2023-05-14 09:13:05 +0530

    use of tags

--- AWS/main.tf
+++ AWS/main.tf
@@ -1,11 +1,17 @@
 resource "aws_instance" "myinstance_east" {
-    provider = selfaws.selfaws_east
-    ami = "ami-005b11f8b84489615"
-    instance_type = "t2.micro"
+  provider      = selfaws.selfaws_east
+  ami           = "ami-005b11f8b84489615"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Otherworld"
+  }
 }
 
 resource "aws_instance" "myinstance_west" {
-  provider = selfaws.selfaws_west
-  ami = "ami-051ed863837a0b1b6"
+  provider      = selfaws.selfaws_east
+  ami           = "ami-005b11f8b84489615"
   instance_type = "t2.micro"
+  tags = {
+    Name = "HelloWorld"
+  }
 }

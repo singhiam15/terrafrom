54e2ad0fcd61cf14ee1cdb67d5f2ddb4a9dd033e
singhiam15 <singhiam@outlook.com>
2023-05-14 09:27:14 +0530

    main file with count

--- AWS/main.tf
+++ AWS/main.tf
@@ -2,16 +2,8 @@ resource "aws_instance" "myinstance_east" {
   provider      = selfaws.selfaws_east
   ami           = "ami-005b11f8b84489615"
   instance_type = "t2.micro"
+ count = 5
   tags = {
     Name = "Otherworld"
-  }
-}
-
-resource "aws_instance" "myinstance_west" {
-  provider      = selfaws.selfaws_east
-  ami           = "ami-005b11f8b84489615"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "HelloWorld"
   }
 }

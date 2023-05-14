994658efe5a8235a5508353d43ee5360177019af
singhiam15 <singhiam@outlook.com>
2023-05-14 09:12:40 +0530

    as date 14052023

--- AWS/versions.tf
+++ AWS/versions.tf
@@ -2,19 +2,22 @@
 terraform {
   required_providers {
     selfaws = {
-        source = "hashicorp/aws"
-        version = "4.67.0"
+      source  = "hashicorp/aws"
+      version = "4.67.0"
     }
+  }
+  backend "local" {
+    path = "./amit/terraform.tfstate"
   }
 }
 provider "selfaws" {
-    region = "us-east-1"
-    profile = "selfamit"
-    alias = "selfaws_east"
+  region  = "us-east-1"
+  profile = "selfamit"
+  alias   = "selfaws_east"
 }
 
 provider "selfaws" {
-    region = "us-west-1"
-    alias = "selfaws_west"
-    profile = "selfamit"
+  region  = "us-west-1"
+  alias   = "selfaws_west"
+  profile = "selfamit"
 }
